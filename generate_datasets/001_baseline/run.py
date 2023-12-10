34aacd78e0aa2e7ccfc801e40ac5d45c5b34cad9
unonao <naokiuno634@gmail.com>
2023-12-10 04:40:48 +0000

    ベースを作成

--- generate_datasets/001_baseline/run.py
+++ generate_datasets/001_baseline/run.py
@@ -13,9 +13,26 @@ import polars as pl
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
 
-import utils
-from utils.load import load_label_data, load_log_data, load_session_data
-from utils.metrics import calculate_metrics
+from utils.data import convert_to_32bit
+from utils.load import load_label_data, load_log_data, load_yad_data
+
+numerical_col = [
+    "total_room_cnt",
+    "wireless_lan_flg",
+    "onsen_flg",
+    "kd_stn_5min",
+    "kd_bch_5min",
+    "kd_slp_5min",
+]
+"""
+categorical_col = [
+    "yad_type",
+    "wid_cd",
+    "ken_cd",
+    "lrg_cd",
+    "sml_cd",
+]
+"""
 
 
 def load_and_union_candidates(cfg, mode: str):
@@ -58,18 +75,67 @@ def load_and_union_candidates(cfg, mode: str):
     return candidate_df
 
 
+def concat_label_fold(cfg, mode: str, candidate_df):
+    """
+    train に対して original, label, fold を付与する
+    validationのスコア計算時にはoriginalを外して計算を行う
+    """
+    if mode == "train":
+        candidate_df = (
+            pl.concat(
+                [
+                    candidate_df.with_columns(
+                        pl.lit(True).alias("original"), pl.lit(False).alias("label")
+                    ),
+                    load_label_data(Path(cfg.dir.data_dir))
+                    .with_columns(
+                        pl.col("yad_no").alias("candidates"),
+                        pl.lit(False).alias("original"),
+                        pl.lit(True).alias("label"),
+                    )
+                    .drop("yad_no"),
+                ]
+            )
+            .group_by(["session_id", "candidates"])
+            .agg(pl.sum("original"), pl.sum("label"))
+        )
+        fold_df = pl.read_parquet(cfg.exp.fold_path)
+        candidate_df = candidate_df.join(fold_df, on="session_id")
+    return candidate_df
+
+
 def concat_session_feature(cfg, mode: str, candidate_df: pl.DataFrame):
     """
+    # TODO: categorical_colの情報もあとで追加する
     session_id, seq_no, yad_no に yado.csv を結合して集約し、セッションに関する特徴量を作成する
     """
-    pass
+    log_df = load_log_data(Path(cfg.dir.data_dir), mode)
+    yad_df = load_yad_data(Path(cfg.dir.data_dir))
+    log_yad_df = log_df.join(yad_df.fill_null(0), on="yad_no")
+    log_yad_df = log_yad_df.group_by(by="session_id").agg(
+        [pl.sum(col).name.suffix("_session_sum") for col in numerical_col]
+        + [pl.min(col).name.suffix("_session_min") for col in numerical_col]
+        + [pl.max(col).name.suffix("_session_max") for col in numerical_col]
+        + [pl.std(col).name.suffix("_session_std") for col in numerical_col]
+    )
+
+    candidate_df = candidate_df.join(log_yad_df, on="session_id")
+
+    return candidate_df
 
 
 def concat_candidate_feature(cfg, mode: str, candidate_df: pl.DataFrame):
     """
+    # TODO: categorical_colの情報もあとで追加する
     candidateの特徴量を抽出する
     """
-    pass
+    yad_df = load_yad_data(Path(cfg.dir.data_dir))
+    candidate_yad_df = candidate_df.join(
+        yad_df.select(["yad_no"] + numerical_col),
+        left_on="candidates",
+        right_on="yad_no",
+    )
+    return candidate_yad_df
 
 
 def concat_session_candidate_feature(cfg, mode: str, candidate_df: pl.DataFrame):
@@ -80,11 +146,20 @@ def concat_session_candidate_feature(cfg, mode: str, candidate_df: pl.DataFrame)
     pass
 
 
-def concat_label_fold(cfg, mode: str, candidate_df: pl.DataFrame):
-    """
-    fold情報とlabel情報を結合する
-    """
-    pass
+def make_datasets(cfg, mode: str):
+    print(f"make_datasets: {mode}")
+    candidate_df = load_and_union_candidates(cfg, mode)
+    print(f"candidate_df: {candidate_df.shape}")
+    candidate_df = concat_label_fold(cfg, mode, candidate_df)
+    print(f"candidate_df: {candidate_df.shape}")
+    candidate_df = concat_session_feature(cfg, mode, candidate_df)
+    print(f"candidate_df: {candidate_df.shape}")
+    candidate_df = concat_candidate_feature(cfg, mode, candidate_df)
+    print(f"candidate_df: {candidate_df.shape}")
+    #  candidate_df = concat_session_candidate_feature(cfg, mode, candidate_df)
+
+    candidate_df = convert_to_32bit(candidate_df)
+    return candidate_df
 
 
 @hydra.main(version_base=None, config_path=".", config_name="config")
@@ -92,9 +167,22 @@ def main(cfg: DictConfig) -> None:
     runtime_choices = HydraConfig.get().runtime.choices
     exp_name = f"{Path(sys.argv[0]).parent.name}/{runtime_choices.exp}"
     print(f"exp_name: {exp_name}")
-    output_path = Path(cfg.dir.cand_unsupervised_dir) / exp_name
+    output_path = Path(cfg.dir.datasets_dir) / exp_name
     print(f"output_path: {output_path}")
     os.makedirs(output_path, exist_ok=True)
+
+    # train
+    train_df = make_datasets(cfg, "train")
+    train_df.write_parquet(output_path / "train.parquet", use_pyarrow=True)
+
+    print("train_df")
+    print(train_df.head(10))
+    print(train_df.shape)
+    print(train_df.columns)
+
+    # test
+    test_df = make_datasets(cfg, "test")
+    test_df.write_parquet(output_path / "test.parquet", use_pyarrow=True)
 
 
 if __name__ == "__main__":
