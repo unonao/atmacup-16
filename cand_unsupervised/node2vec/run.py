55260a170df20e5d9735d26977913ebc99031eee
unonao <naokiuno634@gmail.com>
2023-12-12 00:11:03 +0000

    fix

--- cand_unsupervised/node2vec/run.py
+++ cand_unsupervised/node2vec/run.py
@@ -1,5 +1,30 @@
 """
 node2vecにより候補頂点と類似度を計算する
+┌────────┬─────────────────────┬─────────────────────────────┐
+│ yad_no ┆ neighbor_yad_nos    ┆ cosine_similarity           │
+│ ---    ┆ ---                 ┆ ---                         │
+│ i64    ┆ list[i64]           ┆ list[f64]                   │
+╞════════╪═════════════════════╪═════════════════════════════╡
+│ 1      ┆ [1, 1013, … 12979]  ┆ [1.0, 0.311857, … 0.215071] │
+│ 2      ┆ [2, 1774, … 851]    ┆ [1.0, 0.332525, … 0.229124] │
+│ 3      ┆ [3, 6116, … 9578]   ┆ [1.0, 0.451802, … 0.236896] │
+│ 4      ┆ [4, 11537, … 11299] ┆ [1.0, 0.330503, … 0.218364] │
+│ 5      ┆ [5, 7155, … 12267]  ┆ [1.0, 0.331817, … 0.216423] │
+└────────┴─────────────────────┴─────────────────────────────┘
+(13562, 3)
+
+┌──────────────────────────────────┬───────────────────────┐
+│ session_id                       ┆ candidates            │
+│ ---                              ┆ ---                   │
+│ str                              ┆ list[i64]             │
+╞══════════════════════════════════╪═══════════════════════╡
+│ 000007603d533d30453cc45d0f3d119f ┆ [2395, 7158, … 5046]  │
+│ 0000ca043ed437a1472c9d1d154eb49b ┆ [13535, 3079, … 980]  │
+│ 0000d4835cf113316fe447e2f80ba1c8 ┆ [123, 9128, … 3693]   │
+│ 0000fcda1ae1b2f431e55a7075d1f500 ┆ [8475, 12468, … 7241] │
+│ 000104bdffaaad1a1e0a9ebacf585f33 ┆ [898, 12340, … 12946] │
+└──────────────────────────────────┴───────────────────────┘
+(288698, 2)
 """
 
 import os
@@ -12,6 +37,7 @@ import polars as pl
 import torch
 from hydra.core.hydra_config import HydraConfig
 from omegaconf import DictConfig, OmegaConf
+from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import LabelEncoder
 from torch_geometric.nn import Node2Vec
 from tqdm.auto import tqdm
@@ -20,11 +46,6 @@ import utils
 import wandb
 from utils.load import load_label_data, load_log_data, load_session_data
 from utils.metrics import calculate_metrics
-
-try:
-    from cuml.neighbors import NearestNeighbors
-except ModuleNotFoundError:
-    from sklearn.neighbors import NearestNeighbors
 
 
 def seed_everything(seed: int = 42):
@@ -83,7 +104,7 @@ def my_app(cfg: DictConfig) -> None:
         all_log_df = all_log_df.with_columns(
             pl.col("yad_no").alias("from_yad_no"),
             pl.col("yad_no").shift(-1).alias("to_yad_no"),
-        )
+        ).filter(pl.col("to_yad_no").is_not_null())
 
         # ラベルエンコーディング
         all_log_df = all_log_df.with_columns(
@@ -100,7 +121,7 @@ def my_app(cfg: DictConfig) -> None:
         model = Node2Vec(
             torch.from_numpy(edges).T,
             sparse=True,
-            *OmegaConf.to_container(cfg.exp.node2vec.params, resolve=True),
+            **OmegaConf.to_container(cfg.exp.node2vec.params, resolve=True),
         ).to(device)
         num_workers = 0 if cfg.debug else 4
         loader = model.loader(
@@ -114,14 +135,15 @@ def my_app(cfg: DictConfig) -> None:
         for epoch in range(num_epochs):
             loss = train()
             wandb.log({"loss/node2vec": loss, "epoch": epoch})
+            tqdm.write(f"epoch: {epoch}, loss: {loss}")
 
         vectors = model(torch.arange(model.num_nodes).to(device)).detach().cpu().numpy()
         vectors = vectors / np.linalg.norm(vectors)
         vector_df = pl.DataFrame(
-            pl.Series(
-                name="yad_no", values=le.inverse_transform(np.arange(len(vectors)))
-            ),
-            pl.Series(name="vector", values=vectors.tolist()),
+            {
+                "yad_no": le.inverse_transform(np.arange(len(vectors))),
+                "vector": vectors.tolist(),
+            }
         )
         vector_df.write_parquet(output_path / "vector.parquet")
 
@@ -133,13 +155,16 @@ def my_app(cfg: DictConfig) -> None:
         neighbor_yad_nos = yad_nos[indices]
         cosine_similarity = 1 - dist
         yad2cand_df = pl.DataFrame(
-            pl.Series(name="yad_no", values=yad_nos),
-            pl.Series(name="neighbor_yad_nos", values=neighbor_yad_nos.tolist()),
-            pl.Series(name="cosine_similarity", values=cosine_similarity.tolist()),
+            [
+                pl.Series(name="yad_no", values=yad_nos),
+                pl.Series(name="neighbor_yad_nos", values=neighbor_yad_nos.tolist()),
+                pl.Series(name="cosine_similarity", values=cosine_similarity.tolist()),
+            ]
         )
         yad2cand_df.write_parquet(output_path / "yad2candidate.parquet")
 
         print(yad2cand_df.head())
+        print(yad2cand_df.shape)
 
     """
     候補の作成
@@ -157,22 +182,20 @@ def my_app(cfg: DictConfig) -> None:
                 .agg(pl.all().sort_by("seq_no").last())
                 .sort(by="session_id")
             )
-            candidate_df = (
-                last_log_df.join(yad2cand_df, on="yad_no")
-                .group_by("session_id")
-                .agg(
-                    pl.col("neighbor_yad_nos").alias("candidates"),
-                    # pl.col("cosine_similarity").alias("cosine_similarity"),
-                )
+            candidate_df = last_log_df.join(yad2cand_df, on="yad_no").with_columns(
+                pl.col("neighbor_yad_nos").alias("candidates"),
+                # pl.col("cosine_similarity").alias("cosine_similarity"),
             )
-            candidate_df = session_df.join(
-                candidate_df, on="session_id", how="left"
-            ).with_columns(
-                # candidates が null の場合は空のリストを入れておく
-                pl.when(pl.col("candidates").is_null())
-                .then(pl.Series("empty", [[]]))
-                .otherwise(pl.col("candidates"))
-                .alias("candidates")
+            candidate_df = (
+                session_df.join(candidate_df, on="session_id", how="left")
+                .with_columns(
+                    # candidates が null の場合は空のリストを入れておく
+                    pl.when(pl.col("candidates").is_null())
+                    .then(pl.Series("empty", [[]]))
+                    .otherwise(pl.col("candidates"))
+                    .alias("candidates")
+                )
+                .select(["session_id", "candidates"])
             )
 
             return candidate_df
@@ -184,6 +207,7 @@ def my_app(cfg: DictConfig) -> None:
 
         print("train_candidate_df")
         print(train_candidate_df.head(5))
+        print(train_candidate_df.shape)
         # 保存
         train_candidate_df.write_parquet(output_path / "train_candidate.parquet")
         test_candidate_df.write_parquet(output_path / "test_candidate.parquet")
